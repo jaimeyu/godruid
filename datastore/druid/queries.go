124e8b51d29ed0c3a5d22cdf5438c58ccde0a69e
Peyo Tzolov <ptzolov@accedian.com>
2017-12-18 11:11:00 -0500

    feat queries with new threshold profile
    Update queries to work with the new threshold profile format.

--- datastore/druid/queries.go
+++ datastore/druid/queries.go
@@ -1,6 +1,8 @@
 package druid
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 
 	pb "github.com/accedian/adh-gather/gathergrpc"
@@ -12,7 +14,7 @@ const (
 )
 
 // HistogramQuery - Count of metrics per bucket for given interval.
-func HistogramQuery(tenant string, dataSource string, metric string, granularity string, direction string, interval string, resolution int32, granularityBuckets int32) *godruid.QueryTimeseries {
+func HistogramQuery(tenant string, dataSource string, metric string, granularity string, direction string, interval string, resolution int32, granularityBuckets int32) (*godruid.QueryTimeseries, error) {
 
 	//peyo TODO need to figure out a better way than just appending Histo
 	aggHist := godruid.AggHistoFold("thresholdBuckets", metric+"Histo", resolution, granularityBuckets, "0", "Infinity")
@@ -31,32 +33,53 @@ func HistogramQuery(tenant string, dataSource string, metric string, granularity
 			),
 		},
 		Intervals: []string{interval},
-	}
+	}, nil
 }
 
 // FilterHelper - helper function to select correct druid filter based on
 // a given event and metric
-func FilterHelper(metric string, e *pb.TenantThresholdProfile_EventAttrMap) *godruid.Filter {
+func FilterHelper(metric string, e *pb.TenantThresholdProfile_EventAttrMap) (*godruid.Filter, error) {
 
-	// if e.UpperBound != 0 && e.LowerBound != 0 {
-	// 	return godruid.FilterLowerUpperBound(metric, godruid.NUMERIC, e.LowerBound, e.LowerStrict, e.UpperBound, e.UpperStrict)
-	// }
+	event := e.GetEventAttrMap()
 
-	// if e.UpperBound != 0 {
-	// 	return godruid.FilterUpperBound(metric, godruid.NUMERIC, e.UpperBound, e.UpperStrict)
-	// }
+	upperStrict, err := strconv.ParseBool(event["upperStrict"])
+	if err != nil && event["upperStrict"] != "" {
+		return nil, fmt.Errorf("Invalid value for 'upperStrict' : %v. Must be a boolean", upperStrict)
+	}
 
-	// if e.LowerBound != 0 {
-	// 	return godruid.FilterLowerBound(metric, godruid.NUMERIC, e.LowerBound, e.LowerStrict)
-	// }
+	lowerStrict, err := strconv.ParseBool(event["lowerStrict"])
+	if err != nil && event["lowerStrict"] != "" {
+		return nil, fmt.Errorf("Invalid value for 'lowerStrict' : %v. Must be a boolean", lowerStrict)
+	}
 
-	return godruid.FilterLowerBound(metric, godruid.NUMERIC, 1000, true)
-	// return nil
+	lowerLimit, err := strconv.ParseFloat(event["lowerLimit"], 32)
+	if err != nil && event["lowerLimit"] != "" {
+		return nil, fmt.Errorf("Invalid value for 'lowerLimit' : %v. Must be a number", lowerLimit)
+	}
+
+	upperLimit, err := strconv.ParseFloat(event["upperLimit"], 32)
+	if err != nil && event["upperLimit"] != "" {
+		return nil, fmt.Errorf("Invalid value for 'upperLimit' : %v. Must be a number", upperLimit)
+	}
+
+	if upperLimit != 0 && lowerLimit != 0 {
+		return godruid.FilterLowerUpperBound(metric, godruid.NUMERIC, float32(lowerLimit), lowerStrict, float32(upperLimit), upperStrict), nil
+	}
+
+	if upperLimit != 0 {
+		return godruid.FilterUpperBound(metric, godruid.NUMERIC, float32(upperLimit), upperStrict), nil
+	}
+
+	if lowerLimit != 0 {
+		return godruid.FilterLowerBound(metric, godruid.NUMERIC, float32(lowerLimit), lowerStrict), nil
+	}
+
+	return nil, fmt.Errorf("Unable to consume threshold profile for: %v", metric)
 }
 
 // ThresholdCrossingQuery - Query that returns a count of events that crossed a thresholds for metric/thresholds
 // defined by the supplied threshold profile..
-func ThresholdCrossingQuery(tenant string, dataSource string, metric string, granularity string, interval string, objectType string, direction string, thresholdProfile *pb.TenantThresholdProfile) *godruid.QueryTimeseries {
+func ThresholdCrossingQuery(tenant string, dataSource string, metric string, granularity string, interval string, objectType string, direction string, thresholdProfile *pb.TenantThresholdProfile) (*godruid.QueryTimeseries, error) {
 
 	var aggregations []godruid.Aggregation
 	metrics := strings.Split(metric, ",")
@@ -78,10 +101,14 @@ func ThresholdCrossingQuery(tenant string, dataSource string, metric string, gra
 						if contains(directions, dk) || direction == "" {
 							for ek, e := range d.GetEventMap() {
 								name := tk + "." + mk + "." + ek + "." + dk
+								filter, err := FilterHelper(mk, e)
+								if err != nil {
+									return nil, err
+								}
 								aggregation := godruid.AggFiltered(
 									godruid.FilterAnd(
-										FilterHelper(mk, e),
-										godruid.FilterSelector("obejctType", tk),
+										filter,
+										godruid.FilterSelector("objectType", tk),
 										godruid.FilterSelector("tenantId", tenant),
 										godruid.FilterSelector("direction", dk),
 									),
@@ -91,7 +118,6 @@ func ThresholdCrossingQuery(tenant string, dataSource string, metric string, gra
 									},
 								)
 								aggregations = append(aggregations, aggregation)
-
 							}
 						}
 					}
@@ -106,5 +132,5 @@ func ThresholdCrossingQuery(tenant string, dataSource string, metric string, gra
 		Context:      map[string]interface{}{"timeout": 60000},
 		Aggregations: aggregations,
 		Intervals:    []string{interval},
-	}
+	}, nil
 }

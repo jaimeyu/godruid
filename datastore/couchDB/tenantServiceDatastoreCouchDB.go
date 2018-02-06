4cdcae6334c4d17c39a1dcb713edcbd9c3304af0
Mike Preston <mpreston@its-MacBook-Pro.local>
2018-02-06 10:55:28 -0500

    cleanup a few formatting issues.

--- datastore/couchDB/tenantServiceDatastoreCouchDB.go
+++ datastore/couchDB/tenantServiceDatastoreCouchDB.go
@@ -33,7 +33,7 @@ func CreateTenantServiceDAO() (*TenantServiceDatastoreCouchDB, error) {
 	provDBURL := fmt.Sprintf("%s:%d",
 		result.cfg.GetString(gather.CK_server_datastore_ip.String()),
 		result.cfg.GetInt(gather.CK_server_datastore_port.String()))
-	logger.Log.Debug("Tenant Service CouchDB URL is: ", provDBURL)
+	logger.Log.Debugf("Tenant Service CouchDB URL is: %s", provDBURL)
 	result.server = provDBURL
 
 	return result, nil

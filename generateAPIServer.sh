bbe718b65909ff99239ef9750db06144643d137e
acdn-mpreston <33030320+acdn-mpreston@users.noreply.github.com>
2018-07-12 15:38:41 -0400

    refactor - quick little change to break up the swagger file but have the components re-assembled into one

--- generateAPIServer.sh
+++ generateAPIServer.sh
@@ -3,5 +3,9 @@
 # Generates the API Server from a swagger file
 #=============================================================
 
-    
+# Build up the full swagger file from the component files:
+cd files
+cat swagger/header.yml swagger/paths/*.yml swagger/definitions/header.yml swagger/definitions/definitions-*.yml > swagger.yml
+
+cd ../
 swagger generate server -f files/swagger.yml --exclude-main --exclude-spec -m swagmodels -A gather

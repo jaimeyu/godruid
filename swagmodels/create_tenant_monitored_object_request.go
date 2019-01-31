b3e77cc0a0bd9b011d5475e4a5a20dc0eb7de670
Peyo Tzolov <ptzolov@accedian.com>
2019-01-31 12:37:05 -0500

    *Fix - removed some whitespaces

--- swagmodels/create_tenant_monitored_object_request.go
+++ swagmodels/create_tenant_monitored_object_request.go
@@ -132,7 +132,7 @@ var createTenantMonitoredObjectRequestTypeObjectTypePropEnum []interface{}
 func init() {
 	var res []string
 
-	if err := json.Unmarshal([]byte(`["unknown","flowmeter","twamp-pe","twamp-sf","twamp-sl","cisco-interface", "cisco-node-summary", "pvx", "paa", "regulator", "rfc8321"]`), &res); err != nil {
+	if err := json.Unmarshal([]byte(`["unknown","flowmeter","twamp-pe","twamp-sf","twamp-sl","cisco-interface","cisco-node-summary","pvx","paa","regulator","rfc8321"]`), &res); err != nil {
 		panic(err)
 	}
 	for _, v := range res {
